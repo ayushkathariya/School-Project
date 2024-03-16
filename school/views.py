2ff1e2089fbbb6e42081c45d7257963ac64733af
Ayush Kathariya <ayushkathariya7@gmail.com>
2024-03-16 22:26:29 +0545

    Final Commit

--- school/views.py
+++ school/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'school/home.html')
+
+def contact(request):
+    return render(request, 'school/contact.html')
+
+def about(request):
+    return render(request, 'school/about.html')
